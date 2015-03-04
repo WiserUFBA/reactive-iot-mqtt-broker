cee92e008abacab5fa84d83f45cdcfccdf324a73
Giovanni Baleani <giovibal@gmail.com>
2015-03-04 15:05:06 +0100

    Gio tenant duplicate debugging

--- src/main/java/io/github/giovibal/mqtt/MQTTTopicsManager.java
+++ src/main/java/io/github/giovibal/mqtt/MQTTTopicsManager.java
@@ -65,6 +65,7 @@ public class MQTTTopicsManager {
         if(t3>100) {
             System.out.println("calculateTopicsToPublish: "+ t3 +" millis.");
         }
+        System.out.println(topicsToPublish);
         return topicsToPublish;
     }
 
@@ -109,8 +110,14 @@ public class MQTTTopicsManager {
     }
 
     public String toVertxTopic(String mqttTopic) {
-        String s = tenant +"/"+ mqttTopic;
-        s = s.replaceAll("/+","/"); // remove multiple slashes
-        return s;
+//        String s = mqttTopic;
+//        if(mqttTopic.startsWith("/")) {
+//            s = tenant + mqttTopic;
+//        } else {
+//            s = tenant +"/"+ mqttTopic;
+//        }
+////        s = s.replaceAll("/+","/"); // remove multiple slashes
+//        return s;
+        return mqttTopic;
     }
 }
