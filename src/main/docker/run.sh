a76578ed4d876f40ab955008746bf5bf86d34166
Giovanni Baleani <giovibal@gmail.com>
2015-07-07 14:54:50 +0200

    Tuning of production docker params

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p 1883-1884:1883-1884 -p 11883-11884:11883-11884 --memory=512m --name=mqtt-broker -d -t giovibal/mqtt-broker
+docker run -p 1883-1885:1883-1885 -p 11883-11885:11883-11885 --memory=512m --name=mqtt-broker -d -t giovibal/mqtt-broker
