682b8d105c673285dc5a88b6f7725b691d5b42e2
Giovanni Baleani <giovibal@gmail.com>
2015-07-15 19:26:53 +0200

    Bridge of EventBus over TCP, now with reconnect.

--- src/main/java/io/github/giovibal/mqtt/EventBusBridgeClientVerticle.java
+++ src/main/java/io/github/giovibal/mqtt/EventBusBridgeClientVerticle.java
@@ -1,12 +1,15 @@
 package io.github.giovibal.mqtt;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.eventbus.DeliveryOptions;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.eventbus.MessageConsumer;
 import io.vertx.core.eventbus.MessageProducer;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.net.NetClient;
 import io.vertx.core.net.NetClientOptions;
 import io.vertx.core.net.NetServerOptions;
 import io.vertx.core.net.NetSocket;
@@ -15,16 +18,21 @@ import io.vertx.core.streams.Pump;
 /**
  * Created by giova_000 on 15/07/2015.
  */
-public class EventBusBridgeClientVerticle extends AbstractVerticle {
+public class EventBusBridgeClientVerticle extends AbstractVerticle implements Handler<AsyncResult<NetSocket>> {
+
+    private NetClient netClient;
+    private String remoteBridgeHost;
+    private Integer remoteBridgePort;
+    private String address;
 
     @Override
     public void start() throws Exception {
 
         JsonObject conf = config();
 
-        String address = MQTTSession.ADDRESS;
-        String remoteBridgeHost = conf.getString("remote_bridge_host", "192.168.231.53");
-        Integer remoteBridgePort = conf.getInteger("remote_bridge_port", 7007);
+        remoteBridgeHost = conf.getString("remote_bridge_host", "192.168.231.53");
+        remoteBridgePort = conf.getInteger("remote_bridge_port", 7007);
+        address = MQTTSession.ADDRESS;
 
         // [TCP <- BUS] listen BUS write to TCP
         NetClientOptions opt = new NetClientOptions()
@@ -37,26 +45,37 @@ public class EventBusBridgeClientVerticle extends AbstractVerticle {
 //        System.out.println(opt.getReconnectAttempts());
 //        System.out.println(opt.getReconnectInterval());
 
-        vertx.createNetClient(opt).connect(remoteBridgePort, remoteBridgeHost, netSocketAsyncResult -> {
-            if (netSocketAsyncResult.succeeded()) {
-                NetSocket netSocket = netSocketAsyncResult.result();
-                netSocket.closeHandler(aVoid -> {
-                    Container.logger().error("Bridge Client - closed connection from server"+ netSocket.writeHandlerID());
+        netClient = vertx.createNetClient(opt);
+        netClient.connect(remoteBridgePort, remoteBridgeHost, this);
+    }
+
+    private long connectionTimerID;
+
+    @Override
+    public void handle(AsyncResult<NetSocket> netSocketAsyncResult) {
+        if (netSocketAsyncResult.succeeded()) {
+            vertx.cancelTimer(connectionTimerID);
+            Container.logger().info("Bridge Client - connected to server [" + remoteBridgeHost + ":" + remoteBridgePort +"]");
+            NetSocket netSocket = netSocketAsyncResult.result();
+            netSocket.closeHandler(aVoid -> {
+                Container.logger().error("Bridge Client - closed connection from server" + netSocket.writeHandlerID());
+                connectionTimerID = vertx.setPeriodic(1000, aLong -> {
+                    netClient.connect(remoteBridgePort, remoteBridgeHost, this);
                 });
-                netSocket.exceptionHandler(throwable -> {
-                    Container.logger().error("Bridge Client - Exception: "+ throwable.getMessage(), throwable);
-                });
-                new EventBusNetBridge(netSocket, vertx.eventBus(), address).start();
+            });
+            netSocket.exceptionHandler(throwable -> {
+                Container.logger().error("Bridge Client - Exception: " + throwable.getMessage(), throwable);
+            });
+            new EventBusNetBridge(netSocket, vertx.eventBus(), address).start();
+        } else {
+            String msg = "Bridge Client - not connected to server [" + remoteBridgeHost + ":" + remoteBridgePort +"]";
+            Throwable e = netSocketAsyncResult.cause();
+            if (e != null) {
+                Container.logger().error(msg, e);
+            } else {
+                Container.logger().error(msg);
             }
-            else {
-                String msg = "Bridge Client - cannot connet to server on "+ remoteBridgeHost +":"+ remoteBridgePort;
-                Throwable e = netSocketAsyncResult.cause();
-                if(e!=null)
-                    Container.logger().error(msg, e);
-                else
-                    Container.logger().error(msg);
-            }
-        });
+        }
     }
 
     @Override
